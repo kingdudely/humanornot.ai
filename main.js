948920b20b40829a449f54cd5883664af502d70b
a <102840087+kingdudely@users.noreply.github.com>
2025-05-25 12:43:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
-async function newChat(user_id) {
-    const headers = {
-        "content-type": "application/json"
+/*
+    Default:
+    {
+        "chat_id": string,
+        "chat_time": number,
+        "user_id": string,
+        "created_at": number,
+        "num_participants": number,
+        "partner_id": string,
+        "partner_group": string,
+        "is_my_turn": boolean,
+        "turn_time": number,
+        "is_active": boolean,
+        "messages": array
     }
+*/
+async function newChat(user_id, chat_id) {
+    let URL = "https://api.humanornot.ai/human-or-not/chat/";
 
-    /*
-        Default:
-        {
-            "chat_id": string,
-            "chat_time": number,
-            "user_id": string,
-            "created_at": number,
-            "num_participants": number,
-            "partner_id": string,
-            "partner_group": string,
-            "is_my_turn": boolean,
-            "turn_time": number,
-            "is_active": boolean,
-            "messages": array
-        }
-    */
+    const fetchApi = async (api, body, method) =>
+        (await fetch(URL + api, {
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({ user_id, ...body }),
+            method,
+        })).json();
 
-    let result = await (await fetch("https://api.humanornot.ai/human-or-not/chat/", {
-        headers,
-        body: JSON.stringify({
-            user_id,
-            origin: "decisionBox"
-        }),
-        method: "POST",
-    })).json();
+    const result = chat_id ? { chat_id } : (await fetchApi('', { origin: "decisionBox" }, "POST"));
 
-    const URL = "https://api.humanornot.ai/human-or-not/chat/" + result.chat_id;
+    URL += result.chat_id;
 
     /*
         {
@@ -37,19 +36,8 @@ async function newChat(user_id) {
             "partner_type": string,
         }
     */
-    result.guess = async (partner_type) => // type - bot/human
-        (await fetch(URL + "/guess", {
-            headers,
-            body: JSON.stringify({ user_id, partner_type }),
-            method: "PUT",
-        })).json();
-
-    result.sendMessage = async (text) => // message 
-        (await fetch(URL + "/send-message", {
-            headers,
-            body: JSON.stringify({ user_id, text }),
-            method: "PUT",
-        })).json();
+    result.guess = partner_type => fetchApi('/guess', { partner_type }, "PUT"); // type - bot/human
+    result.sendMessage = text => fetchApi('/send-message', { text }, "PUT"); // message 
 
     return result;
 }
