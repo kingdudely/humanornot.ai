d41612f3fab23a680f97b14a3edb5d2227366c33
a <102840087+kingdudely@users.noreply.github.com>
2025-05-25 14:34:45 -0400

    Update setInnerText.js

--- setInnerText.js
+++ setInnerText.js
@@ -1,5 +1,5 @@
 function setInnerText(innerText) {
-    const textbox = Array.from(document.getElementsByClassName('hon-ue-content')).pop().children[0];
+	const textbox = Array.from(document.getElementsByClassName('hon-ue-content')).pop().children[0];
     
-    textbox.innerText = innerText; 
+	textbox.innerText = new XMLSerializer().serializeToString(document.createTextNode(innerText));
 }
